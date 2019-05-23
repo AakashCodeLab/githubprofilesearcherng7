07d93fd30559fd9599bf2958e9df4d62ef8da542
aakash1801 <singh.aakash1801@gmail.com>
2019-05-23 11:15:55 +0530

    make it better

--- src/app/offline/offline.component.ts
+++ src/app/offline/offline.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-offline',
   templateUrl: './offline.component.html',
-  styleUrls: ['./offline.component.css']
+  styleUrls: ['./offline.component.scss']
 })
 export class OfflineComponent implements OnInit {
 
